61a04ab57f85c6f2e978e8f685f324246db56659
Larissa Ramos <devlarissasr@gmail.com>
2022-11-11 20:20:58 -0300

    first steps

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+# first Python output
+
+print('Hello, Python!')
+
+# Check the Python Version
+
+print(sys.version)
+
+# Print string and error to see the running order
+
+print("This will be printed")
+print("This will cause an error")
+print("This will NOT be printed")
+
+# Type of 2.14
+print(type(2.14))
+
+# Print out the value in variable
+x = 1
+print(x)
